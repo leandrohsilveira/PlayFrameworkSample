4135b5a1ac1d1f6cc1d7ad0417b7182d4caecaed
leandrohsilveira <leandro.hinckel@gmail.com>
2014-07-20 01:14:29 -0300

    CRUD Usuário
    Adicionada edição de usuários

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,30 +7,21 @@ import com.avaje.ebean.Ebean;
 import models.Usuario;
 import play.Logger;
 import play.Routes;
+import play.api.UsefulException;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.db.ebean.Model;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.defaultpages.error;
 import views.vo.PaginacaoVO;
 
 public class Application extends Controller {
 	
-	private static PaginacaoVO<Usuario> montarListaUsuariosVO(DynamicForm form) {
-		String paginaStr = form.bindFromRequest().get("pagina");
-		String resultadosPorPaginaStr = form.bindFromRequest().get("itens-por-pagina");
-		
-		paginaStr = paginaStr != null ? paginaStr : "1";
-		resultadosPorPaginaStr = resultadosPorPaginaStr != null ? resultadosPorPaginaStr : "3";
-		
-		Integer pagina = Integer.valueOf(paginaStr);
-		Integer resultadosPorPagina = Integer.valueOf(resultadosPorPaginaStr);
-		
-		pagina = pagina > 0 ? pagina : 1;
-		
-		
-		resultadosPorPagina = resultadosPorPagina > 0 ? resultadosPorPagina : 1;
+	private static PaginacaoVO<Usuario> montarListaUsuariosVO(Integer pagina, Integer resultadosPorPagina) {
+		pagina = pagina != null && pagina > 0 ? pagina : 1;
+		resultadosPorPagina = resultadosPorPagina != null && resultadosPorPagina > 0 ? resultadosPorPagina : 3;
 		
 		Integer primeiroItem = (pagina - 1) * resultadosPorPagina;
 		Integer contAtivos = Usuario.contarUsuariosAtivos();
@@ -47,21 +38,52 @@ public class Application extends Controller {
 	}
   
     public static Result index() {
-    	PaginacaoVO<Usuario> vo = montarListaUsuariosVO(Form.form());
-        return ok(index.render(vo));
+    	return usuarios(null);
+    }
+    
+    public static Result usuarios(Integer pagina) {
+    	return usuarios(pagina, new Usuario());
+    }
+    
+    private static Result usuarios(Integer pagina, Usuario usuario) {
+    	PaginacaoVO<Usuario> vo = montarListaUsuariosVO(pagina, null);
+    	return ok(index.render(vo, usuario));
+    }
+    
+    public static Result acao() {
+    	String idStr = Form.form().bindFromRequest().get("usuario-selecionado");
+    	String acao = Form.form().bindFromRequest().get("botao-acao");
+    	Logger.debug("Ação: "+acao);
+    	Logger.debug("Usuário selecionado: "+idStr);
+    	try {
+	    	if(idStr != null) {
+    			if("editar".equals(acao)) {
+    				return editar(Long.valueOf(idStr));
+    			} else if("remover".equals(acao)) {
+    				return remover(Long.valueOf(idStr));
+    			}
+	    	}
+	    	return index();
+    	} catch(NumberFormatException e) {
+    		UsefulException exception = new UsefulException(String.format("Não é possível converter o valor '%s' para número!", idStr), e) {
+				private static final long serialVersionUID = 1L;
+			};
+    		return badRequest(error.render(exception));
+    	}
     }
     
     public static Result salvar() {
     	Usuario usuario = Form.form(Usuario.class).bindFromRequest().get();
-    	usuario.save();
+    	if(usuario.getId() == null) {
+    		usuario.save();
+    	} else {
+    		usuario.update();
+    	}
     	return redirect(routes.Application.index());
     }
     
-    public static Result remover() {
-    	String idStr = Form.form().bindFromRequest().get("usuario-selecionado");
-    	Logger.debug("Usuário selecionado: "+idStr);
-    	if(idStr != null) {
-    		Long id = new Long(idStr);
+    public static Result remover(Long id) {
+    	if(id != null) {
     		Usuario usuario = new Model.Finder<Long, Usuario>(Long.class, Usuario.class).byId(id);
     		usuario.setAtivo(false);
     		usuario.update();
@@ -69,12 +91,19 @@ public class Application extends Controller {
     	return redirect(routes.Application.index());
     }
     
+    public static Result editar(Long id) {
+    	Usuario usuario = null;
+    	if(id != null) {
+			usuario = Usuario.finder().byId(id);
+    	}
+    	return usuarios(null, usuario);
+    }
+    
     public static Result javascriptRoutes() {
         response().setContentType("text/javascript");
         return ok(
             Routes.javascriptRouter("jsRoutes",
-                controllers.routes.javascript.Application.salvar(),
-                controllers.routes.javascript.Application.remover()
+                controllers.routes.javascript.Application.salvar()
             )
         );
     }
